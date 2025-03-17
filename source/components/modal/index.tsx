a637a148a220f401b9edf4dd8dbeb7cee2e9e4f8
anandhuremanan <ananduor@gmail.com>
2025-03-17 14:02:42 +0530

    bump version to 0.0.68; update README and enhance modal component styling

--- source/components/modal/index.tsx
+++ source/components/modal/index.tsx
@@ -12,7 +12,8 @@ import { x } from "../icon/iconPaths";
 import { ModalProps } from "./types";
 
 const defaultClasses = {
-  modal: "fixed z-10 overflow-y-auto inset-0 flex items-center justify-center",
+  modal:
+    "fixed z-10 overflow-y-auto inset-0 flex items-center justify-center bg-[#2b2426bd]",
   modalContent: "bg-white m-10 md:w-[80vh] rounded-xl relative",
   modalTitle:
     "p-4 text-lg leading-6 font-medium text-gray-900 flex justify-between items-center",
